14c240b703502c36a1d05eeab0272f9501b5aba5
makerobotics <yann.letzner@gmail.com>
2021-06-29 18:44:50 +0200

    Added random list and EEPROM index to create random activation. Updated to ESP8266 V3.0.0

--- uselessbox.ino
+++ uselessbox.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <EEPROM.h>
 
 #define RELAY           5 // D1
 #define BUTTON          4 // D2
@@ -9,11 +10,14 @@
 #define ENDPOS          20
 #define SERVO_STANDBY   160
 
+#define FORWARD         1
+#define REVERSE         2
+
 #define CMD_READ        1
 #define CMD_WRITE       2
 #define CMD_MODE        3
 #define CMD_SERVO       4
-
+#define CMD_EE_READ     5
 
 #define SCENARIO_INIT        -1
 #define SCENARIO_0            0
@@ -29,6 +33,11 @@
 
 #define TIMEOUT         15000
 
+#define MAX_INDEX       45
+struct { 
+  unsigned int val = 0;
+} eepromdata;
+
 Servo myservo;  // create servo object to control a servo
 int button = 0;
 int btnLock = 0;
@@ -36,6 +45,9 @@ int state = STATE_STANDBY;
 int scenarioStep = 0;
 int pos = SERVO_STANDBY;
 int scenario = SCENARIO_INIT;
+int addr = 0; // only 1 address used to store the random list index
+unsigned int eepromindex = 0;
+int randomValues[] = {0,4,2,1,3,1,2,4,3,1,3,2,0,1,3,0,2,1,4,1,2,1,0,2,4,4,1,1,3,1,0,3,4,2,2,4,1,3,2,4,1,2,3,2,1};
 
 unsigned long timeout = millis() + TIMEOUT;
 int stateMain = STATE_STANDBY;
@@ -45,14 +57,19 @@ int inDiagnosis = 0;
 
 void setup() {
   Serial.begin(115200);
-  randomSeed(analogRead(A0));
-  delay(50);
-  randomSeed(analogRead(A0));
+  //enablePhaseLockedWaveform();
+  EEPROM.begin(4);  //Initialize EEPROM
+
+  Serial.print("EEPROM index: ");
+  EEPROM.get(addr, eepromdata);
+  Serial.println(eepromdata.val);
+  eepromindex = eepromdata.val;
+
   // flush serial line
   while(Serial.available() > 0) {
     char t = Serial.read();
   }
-  myservo.attach(SERVO);        // attaches the servo on GIOxx to the servo object
+  myservo.attach(SERVO, 544, 2400);  // attaches the servo (redefine ranges s they were changed in ESP8266 V3.0.0)
   myservo.write(SERVO_STANDBY); // start servo at STANDBY deg
   delay(200);                   // wait for servo to be in position
   pinMode(BUTTON, INPUT);       // button pin as input (no pull-up)
@@ -67,8 +84,8 @@ void setup() {
   }
 }
 
-void scenarioStepMoveToContact(int delayscenarioStep){
-  pos--;
+void scenarioStepMoveToContact(int delayscenarioStep, int speed){
+  pos -= speed;
   myservo.write(pos);                 // tell servo to go to position in variable 'pos'
   delay(delayscenarioStep);
   if(!isPushed()){
@@ -104,24 +121,25 @@ void scenarioStepWait(){
   }
 }
 
-void scenarioStepMoveToPosition(int target, int delayscenarioStep){
-  if(target>pos) pos++;
-  else pos--;
+void scenarioStepMoveToPosition(int target, int delayscenarioStep, int direction, int speed){
+  if(target>pos) pos += speed;
+  else pos -= speed;
   myservo.write(pos);                 // tell servo to go to position in variable 'pos'
-  delay(delayscenarioStep);                           // waits 5ms for the servo to reach the position
+  delay(delayscenarioStep);           // waits 5ms for the servo to reach the position
   /* Safety */
   if(pos <= ENDPOS || pos >= SERVO_STANDBY){
     Serial.println("Extreme position");
     scenarioStep++;
   }
-  else if(pos == target){
+  else if( ( (pos >= target) && (direction == FORWARD) )
+  || ( (pos <= target) && (direction == REVERSE) ) ){
     Serial.println("Target position");
     scenarioStep++;
   }
 }
 
-void scenarioStepMoveBackStandby(int delayscenarioStep){
-  pos++;
+void scenarioStepMoveBackStandby(int delayscenarioStep, int speed){
+  pos += speed;
   myservo.write(pos);                      // tell servo to go to position in variable 'pos'
   delay(delayscenarioStep);                        // waits for the servo to reach the position
   if(pos>=SERVO_STANDBY){
@@ -159,9 +177,16 @@ void stateStandby(){
 }
 
 void selectScenario(){
-//  if(scenario >= MAX_SCENARIO) scenario = 0;
-//  else scenario++;
-  scenario = (int)random(0, MAX_SCENARIO+1);
+  //if(scenario >= MAX_SCENARIO) scenario = 0;
+  //else scenario++;
+
+  eepromindex ++;
+  if(eepromindex >= MAX_INDEX) eepromindex = 0;
+  scenario = randomValues[eepromindex];
+
+  Serial.print("Current index: ");
+  Serial.println(eepromindex);
+  
 //scenario = 0;
   Serial.print("Current scenario: ");
   Serial.println(scenario);
@@ -170,11 +195,11 @@ void selectScenario(){
 }
 
 void runScenario(){
-  if(scenario == SCENARIO_0) scenario_01(5);
-  else if(scenario == SCENARIO_1) scenario_01(15);
-  else if(scenario == SCENARIO_2) scenario_02();
-  else if(scenario == SCENARIO_3) scenario_03();
-  else if(scenario == SCENARIO_4) scenario_04();
+  if(scenario == SCENARIO_0) scenario_01(0, 8);
+  else if(scenario == SCENARIO_1) scenario_01(0, 4);
+  else if(scenario == SCENARIO_2) scenario_02(0, 8);
+  else if(scenario == SCENARIO_3) scenario_03(0, 8);
+  else if(scenario == SCENARIO_4) scenario_04(0, 6);
   else{
     stateMain = STATE_STANDBY;
     scenario = SCENARIO_0;
@@ -182,6 +207,10 @@ void runScenario(){
 }
 
 void exit(){
+  eepromdata.val = eepromindex;
+  EEPROM.put(addr, eepromdata);
+  EEPROM.commit();    //Store data to EEPROM
+  delay(100);
   digitalWrite(RELAY, 0);       // release the relay as a latch
 }
 
@@ -191,14 +220,14 @@ void delayCountdown(){
 
 /* Scenarios */
 
-void scenario_01(int speedDelay){
+void scenario_01(int speedDelay, int speed){
   switch(scenarioStep){
     case 0:
     default:
-      scenarioStepMoveToContact(speedDelay);
+      scenarioStepMoveToContact(speedDelay, speed);
       break;
     case 1:
-      scenarioStepMoveBackStandby(speedDelay);
+      scenarioStepMoveBackStandby(speedDelay, speed);
       break;
     case 2:
       stateMain = STATE_STANDBY;
@@ -207,21 +236,21 @@ void scenario_01(int speedDelay){
   }
 }
 
-void scenario_02(){
+void scenario_02(int speedDelay, int speed){
   switch(scenarioStep){
     case 0:
     default:
-      scenarioStepMoveToPosition(140, 15);
+      scenarioStepMoveToPosition(140, 15, FORWARD, speed);
       countdown = millis()+1500; // for next scenarioStep
       break;
     case 1:
       delayCountdown();
       break;
     case 2:
-      scenarioStepMoveToContact(5);
+      scenarioStepMoveToContact(speedDelay, speed);
       break;
     case 3:
-      scenarioStepMoveBackStandby(5);
+      scenarioStepMoveBackStandby(speedDelay, speed);
       break;
     case 4:
       stateMain = STATE_STANDBY;
@@ -230,25 +259,25 @@ void scenario_02(){
   }
 }
 
-void scenario_03(){
+void scenario_03(int speedDelay, int speed){
   //Serial.print("Step: ");Serial.println(scenarioStep);
   switch(scenarioStep){
     case 0:
     default:
-      scenarioStepMoveToContact(5);
+      scenarioStepMoveToContact(speedDelay, speed);
       break;
     case 1:
-      scenarioStepMoveToPosition(140, 5);
+      scenarioStepMoveToPosition(140, speedDelay, FORWARD, speed);
       countdown = millis()+5000; // for next scenarioStep
       break;
     case 2:
       scenarioStepWaitForContact(); // scenarioStep +=2 if timeout
       break;
     case 3:
-      scenarioStepMoveToContact(5);
+      scenarioStepMoveToContact(speedDelay, speed);
       break;
     case 4:
-      scenarioStepMoveBackStandby(5);
+      scenarioStepMoveBackStandby(speedDelay, speed);
       break;
     case 5:
       stateMain = STATE_STANDBY;
@@ -257,43 +286,42 @@ void scenario_03(){
   }
 }
 
-void scenario_04(){
+void scenario_04(int speedDelay, int speed){
   //Serial.print("Step: ");Serial.println(scenarioStep);
   switch(scenarioStep){
     case 0:
     default:
-      scenarioStepMoveToPosition(140, 5);
-      break;
+      scenarioStepMoveToPosition(140, speedDelay, FORWARD, speed);
     case 1:
-      scenarioStepMoveToPosition(120, 5);
+      scenarioStepMoveToPosition(120, speedDelay, REVERSE, speed);
       break;
     case 2:
-      scenarioStepMoveToPosition(140, 5);
+      scenarioStepMoveToPosition(140, speedDelay, FORWARD, speed);
       break;
     case 3:
-      scenarioStepMoveToPosition(120, 5);
+      scenarioStepMoveToPosition(120, speedDelay, REVERSE, speed);
       break;
     case 4:
-      scenarioStepMoveToPosition(140, 5);
+      scenarioStepMoveToPosition(140, speedDelay, FORWARD, speed);
       break;
     case 5:
-      scenarioStepMoveToPosition(120, 5);
+      scenarioStepMoveToPosition(120, speedDelay, REVERSE, speed);
       break;
     case 6:
-      scenarioStepMoveToPosition(140, 5);
+      scenarioStepMoveToPosition(140, speedDelay, FORWARD, speed);
       break;
     case 7:
-      scenarioStepMoveToPosition(120, 5);
+      scenarioStepMoveToPosition(120, speedDelay, REVERSE, speed);
       countdown = millis()+2000; // for next scenarioStep
       break;
     case 8:
       scenarioStepWait(); // scenarioStep +=2 if timeout
       break;
     case 9:
-      scenarioStepMoveToContact(5);
+      scenarioStepMoveToContact(speedDelay, speed);
       break;
     case 10:
-      scenarioStepMoveBackStandby(2);
+      scenarioStepMoveBackStandby(0, speed);
       break;
     case 11:
       stateMain = STATE_STANDBY;
@@ -315,7 +343,7 @@ void test(){
     int parameter = Serial.parseInt();
 
     // look for the newline. That's the end of your sentence:
-    if (Serial.read() == '\n') {
+    if (Serial.read() == '\r') {
       Serial.print("Rx: command: "); Serial.print(command);
       Serial.print(", pin: ");Serial.print(pin);
       Serial.print(", parameter: "); Serial.println(parameter);
@@ -347,6 +375,11 @@ void test(){
         Serial.print("SERVO: ");
         Serial.println(parameter);
         myservo.write(parameter);
+      }
+      else if(command == CMD_EE_READ){
+        Serial.print("EEPROM index: ");
+        EEPROM.get(addr, eepromdata);
+        Serial.println(eepromdata.val);
       }
     }
   }
